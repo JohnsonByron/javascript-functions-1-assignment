f53a7fcc95c1759038003842e9547052e67a9634
Kintelya . <johnsonbyron0829@gmail.com>
2023-10-08 15:51:10 -0400

    Add changes as directed in the exercises

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 //After you're done, try looking around at the CSS and HTML to 
 //see if you understand what's going on :)
 
+function numberAdder( num1, num2 ){
+    return num1 + num2 // Adds both value of num1 and num2
+}
+    console.log(numberAdder( 23, 45 )); // Should return the value 68
+    
